88ae0c4ef2a4baab19c1b906ee539d1910248619
Ray A. Conner <ray.a.conner@gmail.com>
2012-11-27 01:12:08 -0500

    suppress some generics warnings

--- src/main/java/com/github/rconner/anansi/Chain.java
+++ src/main/java/com/github/rconner/anansi/Chain.java
@@ -68,6 +68,7 @@ final class Chain<E> implements Iterable<E> {
      * @return
      */
     public static <T> Chain<T> of(T... elements) {
+        @SuppressWarnings( "unchecked" )
         Chain<T> chain = (Chain<T>) EMPTY;
         for (int i = elements.length - 1; i >= 0; i--) {
             chain = new Chain<T>(elements[i], chain);
@@ -161,6 +162,7 @@ final class Chain<E> implements Iterable<E> {
                     }
 
                     @Override
+                    @SuppressWarnings( "unchecked" )
                     public E next() {
                         if (i >= ref.length) {
                             throw new NoSuchElementException();
