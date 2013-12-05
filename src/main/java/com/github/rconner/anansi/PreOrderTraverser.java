3e1c48ef97ff90c9830064c1fdb730232c5e55c0
Ray A. Conner <ray.a.conner@gmail.com>
2013-12-05 01:10:33 -0500

    Add a big comment showing how the code works.

--- src/main/java/com/github/rconner/anansi/PreOrderTraverser.java
+++ src/main/java/com/github/rconner/anansi/PreOrderTraverser.java
@@ -108,3 +108,130 @@ final class PreOrderTraverser<V, E> implements Traverser<V, E> {
         }
     }
 }
+
+/*
+
+This is a step-by-step example of iterating with this traverser. The example graph is below, child order is alphabetic.
+
+          A
+         / \
+        B   C
+       / \ / \
+      E   D   F
+
+The iteration order will be (walks from A to):
+  A, B, D, E, C, D, F
+
+The root walk will be written as:
+  A:-
+walk.to = A in this degenerate case, and there are no steps.
+
+A single step walk may be written as from:->to, echoing the Walk structure of [ from, Stack<steps> ]. For example:
+  B:->D
+
+A multi-step walk will be written as from:[->to, ->to, ...], with steps ordered from stack top to bottom. For example:
+  A:[->E, ->B]
+
+The state of the Iterator is the state of its move stack, which will be written as:
+
+              move.iterator       move.walk.via (move.walk.from is always A)
+  bottom      [ A:- * ]           []
+              [ A:->B, * A:->C ]  []
+              [ B:->D, B:->E * ]  [->B]
+  top         [ ]                 [->E, ->B]
+
+Or as just "empty" if there are no moves in the stack. The "*" in the iterator precedes the next walk to be returned.
+
+In the step-by-step below, colloquial language will be used (push, pop, top) rather than the actual method names.
+
+"advance top and push next move" means:
+  walk = top.iterator.next()
+  push [ children(walk.to), top.walk.append( walk ) ]
+So, advance the top iterator and push a move for its children.
+
+
+@init
+                                    [ * A:- ]           []
+
+next()
+  pop exhausted iterators           no change
+
+  advance top and push next move    [ A:- * ]           []
+                                    [ * A:->B, A:->C ]  []
+
+  return top.walk = A:-
+
+next()
+  pop exhausted iterators           no change
+
+  advance top and push next move    [ A:- * ]           []
+                                    [ A:->B, * A:->C ]  []
+                                    [ * B:->D, B:->E ]  [->B]
+
+  return top.walk = A:[->B]
+
+
+next()
+  pop exhausted iterators           no change
+
+  advance top and push next move    [ A:- * ]           []
+                                    [ A:->B, * A:->C ]  []
+                                    [ B:->D, * B:->E ]  [->B]
+                                    [ ]                 [->D, ->B]
+
+  return top.walk = A:[->D, ->B]
+
+
+next()
+  pop exhausted iterators           [ A:- * ]           []
+                                    [ A:->B, * A:->C ]  []
+                                    [ B:->D, * B:->E ]  [->B]
+
+  advance top and push next move    [ A:- * ]           []
+                                    [ A:->B, * A:->C ]  []
+                                    [ B:->D, B:->E * ]  [->B]
+                                    [ ]                 [->E, ->B]
+
+  return top.walk = A:[->E, ->B]
+
+
+next()
+  pop exhausted iterators           [ A:- * ]           []
+                                    [ A:->B, * A:->C ]  []
+
+  advance top and push next move    [ A:- * ]           []
+                                    [ A:->B, A:->C * ]  []
+                                    [ * C:->D, C:->F ]  [->C]
+
+  return top.walk = A:[->C]
+
+
+next()
+  pop exhausted iterators           no change
+
+  advance top and push next move    [ A:- * ]           []
+                                    [ A:->B, A:->C * ]  []
+                                    [ C:->D, * C:->F ]  [->C]
+                                    [ ]                 [->D, ->C]
+
+  return top.walk = A:[->D, ->C]
+
+
+next()
+  pop exhausted iterators           [ A:- * ]           []
+                                    [ A:->B, A:->C * ]  []
+                                    [ C:->D, * C:->F ]  [->C]
+
+  advance top and push next move    [ A:- * ]           []
+                                    [ A:->B, A:->C * ]  []
+                                    [ C:->D, C:->F * ]  [->C]
+                                    [ ]                 [->F, ->C]
+
+  return top.walk = A:[->F, ->C]
+
+next()
+  pop exhausted iterators           empty
+
+  throw NoSuchElementException
+
+*/
